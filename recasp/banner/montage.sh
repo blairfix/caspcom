6ee5b2b32905202620ff4ef939a48326b2cb478f
Blair Fix <blairfix@gmail.com>
2020-09-09 07:22:49 -0400

    changed montage

--- recasp/banner/montage.sh
+++ recasp/banner/montage.sh
@@ -4,6 +4,10 @@ cd ./pics
 
 montage   -tile x1  -geometry 600x300-120+0 *.jpg  montage.jpg 
 
+#convert montage.jpg   -blur 0x4   montage.jpg
+#convert montage.jpg  -level 0%,100%,0.5   montage.jpg
+convert montage.jpg  -fill black -colorize 50%   montage.jpg
+
 mv montage.jpg ../
 
 

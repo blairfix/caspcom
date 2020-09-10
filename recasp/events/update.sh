ae11ae5c404536ba6cca1072f0ccdb2af1e17bb9
Blair Fix <blairfix@gmail.com>
2020-09-10 10:55:08 -0400

    added events

---
+++ recasp/events/update.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+
+pandoc events.md -o events.html
+
+# replace special characters
+sed -i -e "s/---/\&mdash;/g
+	s/<\/p>/<\/p>\n/g
+	s/<hr \/>/ /g
+	s/--/\&ndash;/g
+	s/\.\.\./\&#8230;/g 
+	s/ '/ \&lsquo;/g
+	s/('/(\&lsquo;/g
+	s/>'/>\&lsquo;/g  
+	s/' /\&rsquo; /g 
+	s/'/\&rsquo;/g" events.html 
+
+
+Rscript merge.R	
+
+
+# switch to firefox
+wmctrl -vxa firefox
+
+# refresh firefox after delay
+sleep 0.2
+xdotool key F5

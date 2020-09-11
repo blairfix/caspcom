ecccee600e112ec02942f8a2786929f0ef374db5
Blair Fix <blairfix@gmail.com>
2020-09-11 16:55:32 -0400

    added edit url

--- recasp/advisory_board/format.R
+++ recasp/advisory_board/format.R
@@ -28,38 +28,56 @@ name = paste(board$first, board$middle, board$last, sep = " ")
 place = board$place
 
 # table
-table_editor = paste('<tr><td style="width:225px">', 
-               "Blair Fix", 
-               '</td><td style="color: #666;">', 
-               "Toronto, Canada", 
-               "</td></tr>")
 
-table_editor = paste(table_editor, collapse = "\n")
-table_editor = paste('<table>', 
-                     '<td></td><td></td>',
-                     table_editor, 
-                     "</table>")
+  # editor
+  table_editor = paste('<tr><td style="width:225px">', 
+                 "Blair Fix", 
+                 '</td><td style="color: #666;">', 
+                 "Toronto, Canada", 
+                 "</td></tr>")
+  
+  table_editor = paste(table_editor, collapse = "\n")
+  table_editor = paste('<table>', 
+                       '<td></td><td></td>',
+                       table_editor, 
+                       "</table>")
 
-
-table_board = paste('<tr><td style="width:225px">', 
-                  name, 
-                  '</td><td style="color: #666;">', 
-                  place, 
-                  "</td></tr>")
-
-
-table_board = paste(table_board, collapse = "\n")
-table_board = paste('<table',
-                    '<td></td><td></td>',
-                    table_board, 
-                    "</table>")
-
-
-page = paste('<h1 style="font-weight: bold;">Advisory Board</h1>',
-			 '<h3 style="font-weight: bold;">Editor</h3>', 
-             table_editor,
-             '<h3 style="font-weight: bold;">Board</h3>',
-             table_board)
+  # founding editor
+  founding_editor = paste('<tr><td style="width:225px">', 
+                          "Tim Di Muzio", 
+                          '</td><td style="color: #666;">', 
+                          "University of Wollongong, Australia", 
+                          "</td></tr>")
+  
+  founding_editor = paste(founding_editor, collapse = "\n")
+  founding_editor = paste('<table>', 
+                       '<td></td><td></td>',
+                       founding_editor, 
+                       "</table>")
+  
+  
+  # board  
+  table_board = paste('<tr><td style="width:225px">', 
+                    name, 
+                    '</td><td style="color: #666;">', 
+                    place, 
+                    "</td></tr>")
+  
+  
+  table_board = paste(table_board, collapse = "\n")
+  table_board = paste('<table',
+                      '<td></td><td></td>',
+                      table_board, 
+                      "</table>")
+  
+  
+  page = paste('<h1 style="font-weight: bold;">Advisory Board</h1>',
+  			       '<h3 style="font-weight: bold;">Editor</h3>', 
+               table_editor,
+  			       '<h3 style="font-weight: bold;">Founding Editor</h3>',
+               founding_editor,
+  			       '<h3 style="font-weight: bold;">Board</h3>',
+               table_board)
 
 
 write.table(page,
